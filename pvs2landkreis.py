317c4eb45cefab51e28be5cad9dea38e63f3ee00
benni <benjaminstefan.rausch@gmail.com>
2020-04-16 19:20:53 +0200

    exception handling

---
+++ pvs2landkreis.py
@@ -0,0 +1,31 @@
+import geopandas as gpd
+import glob
+import pandas as pd
+from shapely import wkt
+
+#SOURCE: https://www.opengeodata.nrw.de/produkte/geobasis/vkg/dvg/dvg1/ (Shapefile)
+#DESCRIPTION https://www.bezreg-koeln.nrw.de/brk_internet/geobasis/verwaltungskarten/verwaltungsgrenzen/index.html
+landkreise = gpd.read_file('landkreise/reprojected_landkreise.shp', encoding = 'utf8', crs = {'init': 'epsg:4326'})
+landkreise = landkreise[['GN', 'geometry']]
+
+pv_all = pd.DataFrame()
+path = '/Users/benni/Desktop/AWS_OpenNRW/'
+for file in glob.glob(path + '*.csv'):
+    print(file)
+    pv_helper = pd.read_csv(file, sep = '\t', names = ['identifier','PV_coords', 'polygon_coords'])
+    pv_all = pv_all.append(pv_helper)
+
+pv_all = pv_all.reset_index(drop = True)
+
+geometry = gpd.GeoSeries(pv_all['PV_coords'].apply(wkt.loads))
+pv_all_points = gpd.GeoDataFrame(geometry = geometry, crs = {'init': 'epsg:4326'})
+
+return_df = gpd.sjoin(pv_all_points, landkreise, how = 'inner', op = 'within')
+
+liste_key_kreis = landkreise['GN'].values.tolist()
+
+for i in range(len(liste_key_kreis)):
+    helper_df_sjoin = return_df[return_df['GN'] == liste_key_kreis[i]]
+    helper_df_pv_filtered = pv_all.iloc[helper_df_sjoin.index, :]
+    helper_df_pv_filtered['Landkreis'] = liste_key_kreis[i]
+    helper_df_pv_filtered.to_csv('PV_Landkreis_{}'.format(liste_key_kreis[i]))
