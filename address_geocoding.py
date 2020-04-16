aa3bdb4b16b8f391d7084e9ef5837afb09991c0b
benni <benjaminstefan.rausch@gmail.com>
2020-04-16 22:29:47 +0200

    try and except block pv2landkreis for wkt loads error filtering automatic updating of OSM address_geocoding

--- address_geocoding.py
+++ address_geocoding.py
@@ -4,11 +4,19 @@ import csv
 import numpy as np
 from geopy.exc import GeocoderTimedOut, GeocoderQuotaExceeded
 import time
-edit = False
+import os
 
 bnetzmstr_nrw = pd.read_csv('/Users/benni/PycharmProjects/DeepSolar_3D/3d_data/bnetzmstr_nrw_filtered.csv', sep = ';')
 #bnetzmstr_nrw['Standort'].str.extract(r'(?P<Street>\D+)\s?(?P<Number>\d+\S*)?\s(?P<Postal>\d{5})\s(?P<City>\D+)$')
 adressen = bnetzmstr_nrw['Standort'].values.tolist()
+
+if os.path.exists("coords.csv"):
+    coords = pd.read_csv('coords.csv', names = ['y,x'])
+    adressen = adressen[coords.shape[0]:]
+else:
+    print('No coords.csv in directory - geocoding starts')
+
+
 
 for i in range(len(adressen)):
     try:
--- pvs2landkreis.py
+++ pvs2landkreis.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import glob
 import pandas as pd
 from shapely import wkt
+from shapely.errors import WKTReadingError
 
 #SOURCE: https://www.opengeodata.nrw.de/produkte/geobasis/vkg/dvg/dvg1/ (Shapefile)
 #DESCRIPTION https://www.bezreg-koeln.nrw.de/brk_internet/geobasis/verwaltungskarten/verwaltungsgrenzen/index.html
@@ -9,7 +10,7 @@ landkreise = gpd.read_file('landkreise/reprojected_landkreise.shp', encoding = '
 landkreise = landkreise[['GN', 'geometry']]
 
 pv_all = pd.DataFrame()
-path = '/Users/benni/Desktop/AWS_OpenNRW/'
+path = 'AWS_OpenNRW/'
 for file in glob.glob(path + '*.csv'):
     print(file)
     pv_helper = pd.read_csv(file, sep = '\t', names = ['identifier','PV_coords', 'polygon_coords'])
@@ -25,10 +26,23 @@ return_df = gpd.sjoin(pv_all_points, landkreise, how = 'inner', op = 'within') #
 liste_key_kreis = landkreise['GN'].values.tolist()
 
 for i in range(len(liste_key_kreis)):
-    helper_df_sjoin = return_df[return_df['GN'] == liste_key_kreis[i]] #filtering of joined DF for a specific Landkreis
-    helper_df_pv_filtered = pv_all.iloc[helper_df_sjoin.index, :] #filtering of whole segmented PVs dataframe for the lines which lie in the Landkreis
-    helper_df_pv_filtered['Landkreis'] = liste_key_kreis[i] #add Landkreis information
-    helper_df_pv_filtered.to_csv('/Users/benni/Desktop/AWS_OpenNRW/Landkreise_filtered/PV_Landkreis_{}.csv'.format(liste_key_kreis[i])) #save as csv
-    geometry = gpd.GeoSeries(helper_df_pv_filtered['polygon_coords']).apply(wkt.loads)
-    helper_gpd_pv_filtered = gpd.GeoDataFrame(helper_df_pv_filtered, geometry=geometry, crs={'init': 'epsg:4326'})
-    helper_gpd_pv_filtered.to_file(driver='ESRI Shapefile', filename="/Users/benni/Desktop/AWS_OpenNRW/Landkreise_filtered_shapefiles/PV_Landkreis_{}.shp".format(liste_key_kreis[i]))
+    try:
+        print(liste_key_kreis[i])
+        helper_df_sjoin = return_df[return_df['GN'] == liste_key_kreis[i]] #filtering of joined DF for a specific Landkreis
+        helper_df_pv_filtered = pv_all.iloc[helper_df_sjoin.index, :] #filtering of whole segmented PVs dataframe for the lines which lie in the Landkreis
+        helper_df_pv_filtered['Landkreis'] = liste_key_kreis[i] #add Landkreis information
+        helper_df_pv_filtered.to_csv('/Users/benni/Desktop/AWS_OpenNRW/Landkreise_filtered/PV_Landkreis_{}.csv'.format(liste_key_kreis[i])) #save as csv
+        geometry_list = helper_df_pv_filtered['polygon_coords'].values.tolist()
+        geometrys = []
+        for geometry in geometry_list:
+            try:
+                geometrys.append(wkt.loads(geometry))
+            except WKTReadingError:
+                geometrys.append('corrupted geometry')
+        helper_df_pv_filtered['geometry'] = pd.Series(geometrys)
+        helper_df_pv_filtered = helper_df_pv_filtered[helper_df_pv_filtered['geometry'] != 'corrupted geometry']
+        geometry = gpd.GeoSeries(helper_df_pv_filtered['geometry']).apply(wkt.loads)
+        helper_gpd_pv_filtered = gpd.GeoDataFrame(helper_df_pv_filtered[['identifier', 'PV_coords', 'Landkreis']], crs={'init': 'epsg:4326'})
+        helper_gpd_pv_filtered.to_file(driver='ESRI Shapefile', filename="/Users/benni/Desktop/AWS_OpenNRW/Landkreise_filtered_shapefiles/PV_Landkreis_{}.shp".format(liste_key_kreis[i]))
+    except ValueError as e:
+        print(e)
