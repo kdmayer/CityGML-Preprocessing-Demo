25ca0b4d58cd43366c031a9152418720a3351e7e
Kevin <kdmayer92@gmail.com>
2022-07-14 19:30:41 -0700

    gml to csv converter

---
+++ citygml_to_csv_conversion.py
@@ -0,0 +1,8 @@
+from lxml import etree
+from shapely.geometry import Polygon
+from geo_utils import Building, ns_bldg, ns_citygml, ns_gml, convert_3D_2D
+
+import pandas as pd
+
+DATA_DIRECTORY = "/Users/kevin/Projects/CityGML_Processing/GML"
+
